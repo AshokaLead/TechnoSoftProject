88f04cac14df3748c423ec1369e7f2fded386095
ashoka <ashoka.devara89@gmail.com>
2020-03-29 12:32:05 +0530

    TehcnoSoftProject First Commit....

---
+++ src/test/java/com/ashoka/TechnoSoftProject/TechnoSoftProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashoka.TechnoSoftProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnoSoftProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
